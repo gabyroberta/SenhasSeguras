967c2a5485b9ea630896002d4513e446a4f2ba39
gabyroberta <gabrielly.roberta.costa@escola.pr.gov.br>
2026-07-07 17:16:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,35 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick= aumentaTamanho;
 
-function diminuiTamanho() {
-    tamanhoSenha = tamanhoSenha-1;
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        tamanhoSenha--;
+    }
+    numeroSenha.TextCont =tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho(){
+    if (tamanhoSenha > 20){
+        tamanhoSenha++;
+    }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+
+function geraSenha(){
+    let senha = '';
+    for(let i=0; i < tamanhoSenha;i++){
+    let numeroAleatorio = Math.random()*letrasMaiuscula.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + letrasMaiuscula[numeroAleatorio];
+}
+campoSenha.value = senha;
 }
